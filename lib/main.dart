5e71cdd2dd6707287c8a6069cf7437cb40cd6610
mochamadaufa7 <mochamadaufa7@gmail.com>
2024-07-02 07:40:11 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,241 @@
+// import 'dart:nativewrappers/_internal/vm/lib/core_patch.dart';
+
+
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // TRY THIS: Try running your application with "flutter run". You'll see
+          // the application has a purple toolbar. Then, without quitting the app,
+          // try changing the seedColor in the colorScheme below to Colors.green
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+          useMaterial3: true,
+        ),
+        home: FlutterSplashScreen(
+          // useImmersiveMode: true,
+          duration: const Duration(milliseconds: 4000),
+          nextScreen: const MyHomePage(),
+          backgroundColor: Colors.white,
+          splashScreenBody: Center(
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                const Text(
+                  "Tugas Personal",
+                  style: TextStyle(color: Colors.black, fontSize: 24),
+                ),
+                const Spacer(),
+                SizedBox(
+                  width: 200,
+                  child: Lottie.asset(
+                    "assets/duck.json",
+                    repeat: false,
+                  ),
+                ),
+                const Spacer(),
+                const Text(
+                  "Flutter is Love",
+                  style: TextStyle(color: Colors.pink, fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title = 'Home';
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<Map<String, dynamic>> items = [
+    {
+      'title': 'Title 1',
+      'image':
+          'https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp',
+      'description': 'Description 1',
+    },
+    {
+      'title': 'Title 2',
+      'image':
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuWXzkIYkMaVY24lJLPWFRS3WKI3kB3Ls0bg&s',
+      'description': 'Description 2',
+    },
+    {
+      'title': 'Title 3',
+      'image':
+          'https://d3phaj0sisr2ct.cloudfront.net/site/images/tools/thumbnails/reviewed/upscale+image.png',
+      'description': 'Description 3',
+    },
+  ];
+
+  int counter = 0;
+
+  void add() {
+    setState(() {
+      counter ++;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            child: Text(
+              "Jumlah Counter: ${counter}",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    // Navigate to the detail page when the ListTile is tapped.
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailPage(
+                            title: items[index]['title'], data: items[index], stateFun: add),
+                      ),
+                    );
+                  },
+                  child: ListTile(
+                    leading: Image.network(items[index]['image']),
+                    title: Text(items[index]['title']),
+                    subtitle: Text(items[index]['description']),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({super.key, required this.title, required this.data, required this.stateFun});
+
+  final String title;
+  final Map<String, dynamic> data; 
+  final void Function() stateFun;
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 200, 
+              width: 200,  
+              child: Image.network(widget.data['image'], fit: BoxFit.cover), // Gunakan BoxFit.cover agar gambar menutupi seluruh SizedBox
+            ), 
+            Text(
+              widget.title,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                // Ganti dengan deskripsi yang sesuai
+                '${widget.data['description']}', 
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                widget.stateFun();
+                Navigator.pop(context);
+              }, 
+              child: Text('Add'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
